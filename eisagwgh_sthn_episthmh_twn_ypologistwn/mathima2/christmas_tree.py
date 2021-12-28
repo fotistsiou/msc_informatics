0063d5e670419f5000fe6906be0f93a395eb3b05
fotistsiou <fotis.tsioumas@outlook.com>
2021-12-28 17:31:19 +0200

    2nd lecture

---
+++ eisagwgh_sthn_episthmh_twn_ypologistwn/mathima2/christmas_tree.py
@@ -0,0 +1,22 @@
+# Guidelines #
+
+# In order to run this code, have to follow below steps:
+# 1. Open the terminal
+# 2. Run: cd/<file_path>
+# 3. Run: python3 <filename.py>
+
+# Start Code #
+
+# Python Christmas Tree #
+
+# Hardcoded Christmas Tree
+for i in range(10):
+#    print(2*i+1) # fisrt step
+#    print((2*i+1)*"*") # second step 
+   print((9-i)*" "+(2*i+1)*"*") # last step
+
+# Christmas Tree using variables
+h = 20
+c = "&"
+for i in range(h):
+   print((h-i)*" "+(2*i+1)*c)
