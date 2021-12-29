690760c0c9126a1201802d89a6154b187d75cd67
fotistsiou <fotis.tsioumas@outlook.com>
2021-12-29 20:43:46 +0200

    3rd lecture arxes_ptogrammatismou_c_c++

---
+++ arxes_programmatismou_c_c++/mathima3/break_continue_return.c
@@ -0,0 +1,96 @@
+// Guidelines //
+
+// In order to run this code, have to follow below steps:
+// 1. Open the terminal
+// 2. Run: cd/<file_path>
+// 3. Run: gcc <c_file.c> -o <executable_filename>
+
+// Start Code //
+
+// ------------------------ HEADER SECTION ------------------------ //
+
+#include <stdio.h>
+
+// ------------------------ MAIN SECTION ------------------------ //
+
+int main() {
+
+   // --- BREAK --- //
+
+   // one break //
+
+   int i;
+   int j;
+   for (i = 0; i <= 4; i++) {
+      for (j = 0; j <= 4; j++) {
+        if (j == 2) break;
+        printf("i = %d, and j = %d\n", i, j);
+      }
+    }
+
+   // two break //
+
+   int i;
+   int j;
+   for (i = 0; i <= 4; i++) {
+       if (i == 2) break;
+       for (j = 0; j <= 4; j++) {
+           if (j == 2) break;
+           printf("i = %d, and j = %d\n", i, j);
+       }
+   }
+
+
+   // --- CONTINUE --- //
+
+   // one continue //
+
+   int i;
+   int j;
+   for (i = 0; i <= 4; i++) {
+       for (j = 0; j <= 4; j++) {
+           if (j == 2) continue;
+           printf("i = %d, and j = %d\n", i, j);
+       }
+   }
+
+   // two continue //
+
+   int i;
+   int j;
+   for (i = 0; i <= 4; i++) {
+       if (i == 2) continue;
+       for (j = 0; j <= 4; j++) {
+           if (j == 2) continue;
+           printf("i = %d, and j = %d\n", i, j);
+       }
+   }
+
+   // --- RETURN --- //
+
+   // one return //
+
+   int i;
+   int j;
+   for (i = 0; i <= 4; i++) {
+       for (j = 0; j <= 4; j++) {
+           if (j == 2) return 0;
+           printf("i = %d, and j = %d\n", i, j);
+       }
+   }
+
+   // two return //
+
+   int i;
+   int j;
+   for (i = 0; i <= 4; i++) {
+       if (i == 2) return 0;
+       for (j = 0; j <= 4; j++) {
+            if (j == 2) return 0;
+            printf("i = %d, and j = %d\n", i, j);
+       }
+   }
+
+
+    return 0;
+}
---
+++ arxes_programmatismou_c_c++/mathima3/switch.c
@@ -0,0 +1,38 @@
+// Guidelines //
+
+// In order to run this code, have to follow below steps:
+// 1. Open the terminal
+// 2. Run: cd/<file_path>
+// 3. Run: gcc <c_file.c> -o <executable_filename>
+
+// Start Code //
+
+// ------------------------ HEADER SECTION ------------------------ //
+
+#include <stdio.h>
+
+// ------------------------ MAIN SECTION ------------------------ //
+
+int main() {
+
+    int num;
+    printf("Give a number from 1 to 3: ");
+    scanf("%d", &num);
+    switch (num) {
+        case 1:
+            printf("Your number is: %d", num);
+            break;
+        case 2:
+            printf("Your number is: %d", num);
+            break;
+        case 3:
+            printf("Your number is: %d", num);
+            break;
+        default:
+            printf("Your number is not from 1 to 3");
+            break;
+    }
+
+
+    return 0;
+}
