0844ede06f7daaf8c4ee18f1b59bdff74860cf07
fotistsiou <fotis.tsioumas@outlook.com>
2021-12-29 16:48:35 +0200

    1st lecture arxes_programmatismou_c_c++

---
+++ arxes_programmatismou_c_c++/mathima1/main.c
@@ -0,0 +1,19 @@
+// Guidelines //
+
+// In order to run this code, have to follow below steps:
+// 1. Open the terminal
+// 2. Run: cd/<file_path>
+// 3. Run: gcc <c_file.c> -o <executable_filename>
+
+// Start Code //
+
+// ---------- Header Section ---------- //
+
+#include <stdio.h> // C library which has the necessary information to include the input/output related functions in our program. Example printf, scanf etc.
+
+// ---------- Main Section ---------- //
+
+int main() { // "int": type function, "main()": the first function of every C program that is responsible for starting the execution and termination of the program.
+    printf("Hello, World!\n"); // "printf()": function which used for output in C language, "\n": new line
+    return 0; // The return value of main() function. A return 0 means that the program will execute successfully and did what it was intended to do.
+}
