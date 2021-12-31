d54ccc0a9b87d7fa2fdb2674ac63b19f6c08c20e
fotistsiou <fotis.tsioumas@outlook.com>
2021-12-31 12:17:18 +0200

    3rd lecture arxes_programmatismou_c_c++

--- arxes_programmatismou_c_c++/mathima3/arrays.c
+++ arxes_programmatismou_c_c++/mathima3/arrays.c
@@ -46,6 +46,17 @@ int main() {
         }
     }
 
+    // Program to find the average of 10 numbers which a user give you
+    int i, arr[10], sum=0;
+    float avg = 0.0;
+    printf("Enter 10 numbers:\n");
+    for(i = 0; i < 10; i++) {
+        scanf("%d", &arr[i]);
+        sum = sum + arr[i];
+    }
+    avg=(float)sum/10;
+    printf("Average = %f", avg);
+
 
     return 0;
 }
