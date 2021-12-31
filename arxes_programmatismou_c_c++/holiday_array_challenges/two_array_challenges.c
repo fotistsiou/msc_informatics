5ce99e96025c8c3324f45b551faba04b4c38a5d4
fotistsiou <fotis.tsioumas@outlook.com>
2021-12-31 14:44:20 +0200

    Holiday Array Challenges

---
+++ arxes_programmatismou_c_c++/holiday_array_challenges/two_array_challenges.c
@@ -0,0 +1,22 @@
+// Guidelines //
+
+// In order to run this code, have to follow below steps:
+// 1. Open the terminal
+// 2. Run: cd/<file_path>
+// 3. Run: gcc <c_file.c> -o <executable_filename>
+
+// Start Code //
+
+// ------------------------ HEADER SECTION ------------------------ //
+
+#include <stdio.h>
+
+// ------------------------ MAIN SECTION ------------------------ //
+
+int main() {
+
+    
+
+
+    return 0;
+}
