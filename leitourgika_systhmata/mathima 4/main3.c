6942049da9c57ba9ae0dd081f78b4cdaecb889c8
fotistsiou <fotis.tsioumas@outlook.com>
2022-02-05 21:56:05 +0200

    4th lecture leitourgika_systhmata

--- leitourgika_systhmata/mathima 4/main3.c
+++ leitourgika_systhmata/mathima 4/main3.c
@@ -1,3 +1,11 @@
+// Guidelines from Fotis K. Tsioumas //
+
+// In order to run this code, have to follow below steps:
+// 1. Open the terminal
+// 2. Run: cd/<file_path>
+// 3. Run: gcc <c_file.c> -o <executable_filename>
+
+// Start Code //
 #include <stdio.h>
 #include <unistd.h> 
 
