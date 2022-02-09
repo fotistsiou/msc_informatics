83aea178f22859af191b4785e31b2988feb1f7c0
fotistsiou <fotis.tsioumas@outlook.com>
2022-02-10 00:40:32 +0200

    4th lecture eisagwgh_sthn_episthmh_twn_ypologistwn

--- eisagwgh_sthn_episthmh_twn_ypologistwn/mathima4/Kino API/kino4.py
+++ eisagwgh_sthn_episthmh_twn_ypologistwn/mathima4/Kino API/kino4.py
@@ -7,7 +7,7 @@
 
 # Start Code #
 
-# Fetch Data and Print Numbers of Last 10 Draws #
+# Fetch Data and Print How Many Times Each Number is displayed in the Last 10 Draws #
 import urllib.request
 import json
 
@@ -17,10 +17,13 @@ data_in_python = json.loads(data_in_json)
 win_numbers = data_in_python["last"]["winningNumbers"]["list"]
 last_drawId = data_in_python["last"]["drawId"]
 
-# Loop for last 9 draws and add them to last draw from above
 for i in range(9):
     with urllib.request.urlopen("https://api.opap.gr/draws/v3.0/1100/"+str(last_drawId-i-1)) as response:
         data_in_json = response.read()
     data_in_python = json.loads(data_in_json)
     win_numbers += data_in_python["winningNumbers"]["list"]
 print("Numbers of 10 Last Draws :",win_numbers)
+
+# Print how many times each number is displayed in the last 10 draws
+for i in range(1,81):
+    print("Number ",i," display ", win_numbers.count(i), "times in the Last 10 Draws")
