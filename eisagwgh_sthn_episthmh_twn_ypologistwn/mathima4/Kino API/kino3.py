2b8eecde6260768eb67ce3aaee35298c6276669a
fotistsiou <fotis.tsioumas@outlook.com>
2022-02-10 00:28:06 +0200

    4th lecture eisagwgh_sthn_episthmh_twn_ypologistwn

---
+++ eisagwgh_sthn_episthmh_twn_ypologistwn/mathima4/Kino API/kino3.py
@@ -0,0 +1,26 @@
+# Guidelines from Fotis K. Tsioumas # 
+
+# In order to run this code, have to follow below steps:
+# 1. Open the terminal
+# 2. Run: cd/<file_path>
+# 3. Run: python3 <filename.py>
+
+# Start Code #
+
+# Fetch Data and Print Numbers of 10 Last Draws #
+import urllib.request
+import json
+
+with urllib.request.urlopen("https://api.opap.gr/draws/v3.0/1100/last-result-and-active") as response: 
+    data_in_json = response.read()
+data_in_python = json.loads(data_in_json)
+win_numbers = data_in_python["last"]["winningNumbers"]["list"]
+last_drawId = data_in_python["last"]["drawId"]
+
+# Loop for 9 last draws and add them to last draw from above
+for i in range(9):
+    with urllib.request.urlopen("https://api.opap.gr/draws/v3.0/1100/"+str(last_drawId-i-1)) as response:
+        data_in_json = response.read()
+    data_in_python = json.loads(data_in_json)
+    win_numbers += data_in_python["winningNumbers"]["list"]
+print("Numbers of 10 Last Draws :",win_numbers)
