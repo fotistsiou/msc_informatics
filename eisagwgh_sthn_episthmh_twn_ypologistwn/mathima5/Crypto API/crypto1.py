a8ce187e1d4110df87b62b4cc21f535aa0a4559c
fotistsiou <fotis.tsioumas@outlook.com>
2022-02-20 21:09:21 +0200

    5th lecture eisagwgh_sthn_episthmh_twn_ypologistwn

--- eisagwgh_sthn_episthmh_twn_ypologistwn/mathima5/Crypto API/crypto1.py
+++ eisagwgh_sthn_episthmh_twn_ypologistwn/mathima5/Crypto API/crypto1.py
@@ -6,3 +6,22 @@
 # 3. Run: python3 <filename.py>
 
 # Start Code #
+
+# Fetch Bitcoin Prices in Euros, 10 Times and Print a Graph of These Prices #
+from matplotlib import pyplot as plt
+import urllib.request
+import json
+from time import sleep
+
+btc_prices=[]
+url="https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC&tsyms=EUR&e=CCCAGG"
+for i in range(10):
+    with urllib.request.urlopen(url) as response:
+        html = response.read()
+    data = json.loads(html)
+    btc_prices.append(data['BTC']['EUR'])
+    sleep(1)
+
+fig = plt.figure(figsize=(10, 7))
+plt.bar(list(range(10)),btc_prices)
+plt.show()
