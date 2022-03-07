dfb545fd2d434cba85a95ba8646f4268a4f28fd8
fotistsiou <fotis.tsioumas@outlook.com>
2022-03-07 02:28:47 +0200

    Exam Challenge 2021-2022

--- 1st_semester/arxes_programmatismou_c_c++/exam_challenge_2021-2022/linked_list_challenge.c
+++ 1st_semester/arxes_programmatismou_c_c++/exam_challenge_2021-2022/linked_list_challenge.c
@@ -11,10 +11,13 @@ struct patient {
 };
 
 struct patient *head;
+int count; 
+
 
 void add(int amka, char name[40], int age, char gender[40]) {
 	struct patient *pts;
 	pts = malloc(sizeof(struct patient));
+	count++;
 	pts->amka = amka;
 	strcpy(pts->name, name);
 	pts->age = age;
@@ -29,12 +32,68 @@ void add(int amka, char name[40], int age, char gender[40]) {
 }
 
 void generate_arrays() {
-	
+	// struct patient *pts;
+	// pts = head;
+	// int men_arr[count];
+	// int women_arr[count];
+	// int len_men_arr = sizeof(men_arr)/sizeof(men_arr[0]);
+	// int len_women_arr = sizeof(women_arr)/sizeof(women_arr[0]);
+	// if (pts == NULL){
+    //     printf("Not exist patients in the list!\n");
+    //     return;
+	// }
+	// while (pts != NULL) {
+	// 	if (pts->gender == "MAN") {
+	// 		for (int i = 0; i < len_men_arr; i++) {
+	// 			men_arr[i] = pts->age;
+	// 		}
+	// 	} else if (pts->gender == "WOMAN") {
+	// 		for (int j = 0; j < len_women_arr; j++) {
+	// 			men_arr[j] = pts->age;
+	// 		}
+	// 	}
+	// 	pts = pts->next;
+	// 	for (int i = 0; i < len_men_arr; i++) {
+	// 			printf("%d\n", men_arr[i]);
+	// 	}
+	// 	for (int j = 0; j < len_women_arr; j++) {
+	// 			printf("%d\n", men_arr[j]);
+	// 	}
+	// }
 }
 
+int men_arr_test[10] = {3, 4, 9 , 45, 20, 80, 99, 50, 77, 19};
+int women_arr_test[10] = {3, 4, 9 , 45, 20, 80, 99, 50, 77, 19};
 
 void print_max_min() {
-	
+	int len_men_arr_test = sizeof(men_arr_test)/sizeof(men_arr_test[0]);
+	int len_women_arr_test = sizeof(women_arr_test)/sizeof(women_arr_test[0]);
+	int max1 = 0;
+	int max2 = 0;
+	for (int i = 0; i < len_men_arr_test; i++){
+		if (men_arr_test[i] > max2) {
+			if (men_arr_test[i] >= max1){
+				max1 = men_arr_test[i];
+			} else {
+				max2 = men_arr_test[i];
+			}
+		}
+	}
+	int min1 = max1;
+	int min2 = max2;
+	for (int i = 0; i < len_women_arr_test; i++){
+		if (women_arr_test[i] < min2) {
+			if (women_arr_test[i] <= min1){
+				min1 = women_arr_test[i];
+			} else {
+				min2 = women_arr_test[i];
+			}
+		}
+	}
+	printf("Max 1: %d\n", max1);
+	printf("Max 2: %d\n", max2);
+	printf("Min 1: %d\n", min1);
+	printf("Min 2: %d\n", min2);
 }
 
 int main() {
@@ -48,7 +107,7 @@ int main() {
 		printf("Give action number:\n");
 		scanf("%d", &num);
 		if (num == 1) {
-			printf("Enter the age:");
+			printf("Enter the amka:");
             scanf("%d", &amka);
 			printf("Enter the name:");
             scanf("%s", name);
