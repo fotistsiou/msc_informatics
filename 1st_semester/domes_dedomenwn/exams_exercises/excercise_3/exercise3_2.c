5ddb34382fb8158d5a329a918f6d4261e56c48ab
fotistsiou <fotis.tsioumas@outlook.com>
2022-03-12 23:31:04 +0200

    exams_exercises_domes_dedomenwn

--- 1st_semester/domes_dedomenwn/exams_exercises/excercise_3/exercise3_2.c
+++ 1st_semester/domes_dedomenwn/exams_exercises/excercise_3/exercise3_2.c
@@ -22,34 +22,33 @@ struct student {
 
 int main() {
 
-    struct student s[2];
+    int num;
+    printf("Give the number of students\n");
+    scanf("%d", &num);
+    struct student s[num];    
 
-    printf("Enter Records of 2 students\n");    
-
-    for (int i = 0; i < 2; i++) {
+    for (int i = 0; i < num; i++) {
         printf("%d Student\n", i+1);
         printf("Enter am:");
         scanf("%d", &s[i].am);
         printf("Enter name:");
-        scanf("%s", &s[i].name);
+        scanf("%s", s[i].name);
         printf("Enter surname:");
-        scanf("%s", &s[i].surname);
+        scanf("%s", s[i].surname);
         printf("Enter father name:");
-        scanf("%s", &s[i].father_name);
+        scanf("%s", s[i].father_name);
         printf("Enter address:");
-        scanf("%s", &s[i].address);
+        scanf("%s", s[i].address);
         printf("Enter telephone:");
         scanf("%d", &s[i].telephone);
         printf("Enter mobile:");
         scanf("%d", &s[i].mobile);
         printf("Enter lesson:");
-        scanf("%s", &s[i].lesson);
+        scanf("%s", s[i].lesson);
     }
     
-    for (int i = 0; i < 2; i++) {
-        printf("******************************************************\n");
+    for (int i = 0; i < num; i++) {
         printf("Student: %d, %s %s, %s, %s, %d, %d, %s\n", s[i].am, s[i].name, s[i].surname, s[i].father_name, s[i].address, s[i].telephone, s[i].mobile, s[i].lesson);
-        printf("******************************************************\n");
     }
 
     return 0;
---
+++ 1st_semester/domes_dedomenwn/exams_exercises/excercise_3/exercise3_3.c
@@ -0,0 +1,101 @@
+// Guidelines from Fotis K. Tsioumas//
+
+// In order to run this code, have to follow below steps:
+// 1. Open the terminal
+// 2. Run: cd/<file_path>
+// 3. Run: gcc <c_file.c> -o <executable_filename>
+
+// Start Code //
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct student {
+    int am;
+    char name[30];
+    char surname[30];
+    char father_name[30];
+    char address[50];
+    int telephone;
+    int mobile;
+    char lesson[30];
+    struct student *next;
+};
+
+struct student *head;
+
+void add(int am, char name[30], char surname[30], char father_name[30], char address[50], int telephone, int mobile, char lesson[30]) {
+    struct student *tmp;
+    tmp = malloc(sizeof(struct student));
+    tmp->am = am;
+    strcpy(tmp->name, name);
+    strcpy(tmp->surname, surname);
+    strcpy(tmp->father_name, father_name);
+    strcpy(tmp->address, address);
+    tmp->telephone = telephone;
+    tmp->mobile = mobile;
+    strcpy(tmp->lesson, lesson);
+    if (head == NULL) {
+        tmp->next = NULL;
+        head = tmp;
+    } else {
+        tmp->next = head;
+        head = tmp;
+    }
+}
+
+void find(char les[50]) {
+    struct student *tmp;
+    int result;
+    tmp = head;
+    if (tmp == NULL) {
+        return;
+    }
+    while (tmp != NULL) {
+        result = strcmp(les, tmp->lesson);
+        if (result == 0) {
+            printf("AM: %d\n", tmp->am);
+        } 
+        tmp = tmp->next;
+    }
+}
+
+int main() {
+    int num, am, telephone, mobile;
+    char name[30], surname[30], father_name[30], address[50], lesson[30], les[50];
+    while (1) {
+        printf("1.Add Student\n");
+        printf("2.Find Students who choiced a specific lesson\n");
+        printf("3.Exit\n");
+        printf("Enter the action number:");
+        scanf("%d", &num);
+        if (num == 1) {
+            printf("Enter the student's am:");
+            scanf("%d", &am);
+            printf("Enter the student's name:");
+            scanf("%s", name);
+            printf("Enter the student's surname:");
+            scanf("%s", surname);
+            printf("Enter the student's father name:");
+            scanf("%s", father_name);
+            printf("Enter the student's address:");
+            scanf("%s", address);
+            printf("Enter the student's telephone:");
+            scanf("%d", &telephone);
+            printf("Enter the student's mobile:");
+            scanf("%d", &mobile);
+            printf("Enter the student's lesson:");
+            scanf("%s", lesson);
+            add(am, name, surname, father_name, address, telephone, mobile, lesson);
+        }
+        else if (num == 2) {
+            printf("Enter the lesson:");
+            scanf("%s", les);
+            find(les);
+        }
+        else if (num == 3) {
+            return 0;
+        }
+    }
+}
