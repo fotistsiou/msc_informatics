743583b5b120bac95b62954dab752e424a41d6a4
fotistsiou <fotis.tsioumas@outlook.com>
2022-03-22 00:21:27 +0200

    exams_exercise_glwssws_programmatismou_kai_metaglwttistes

---
+++ 1st_semester/glwssws_programmatismou_kai_metaglwttistes/exams_exercises/exercise1/exercise1.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <string.h>
+
+#define max 51
+
+struct stack {
+	char st[max];
+	int top;
+} stk;
+
+void push(char);
+int pop();
+void print_state();
+
+int main() {
+	char s[max];
+	int i;
+	stk.top = -1;
+    printf("Enter a sequence of 92 max 25 times:\n");
+	scanf("%s",s);
+	int l = strlen(s);
+	for(i = 0; i < l; i++) {
+		if(s[i] == '9') {
+			printf("Push!\n");
+			push(s[i]);
+		} else if(s[i] == '2') {
+                if(stk.st[stk.top] == '9') {
+					printf("Pop!\n");
+                    pop();
+                } else {
+					printf("Fail!\n\n");
+                    stk.top = 0;
+                    break;
+                }
+		}
+		print_state(s, i);
+		printf("\n");
+	}
+    if(stk.top == -1) {
+        printf("Final State: Balanced!\n");
+    } else {
+        printf("Final State: Unbalanced.\n");
+    }
+}
+
+void push(char item) {
+	if(stk.top == max-1) {
+		printf("Stack Is Full.\n");
+	} else {
+		stk.top = stk.top+1;
+		stk.st[stk.top] = item;
+	}
+}
+
+int pop() {
+	if(stk.top == -1) {
+		printf("Stack Is Empty.\n");
+	} else {
+		stk.top = stk.top-1;
+	}
+}
+
+void print_state(char s[], int index) {
+	printf("New character: %c\n", s[index]);
+	printf("Rest of the Input: %s\n", s+index+1);
+	for (int i = 0; i <= stk.top; i++) {
+		printf("| %c ", stk.st[i]);
+	}
+	printf("<-\n");
+}
