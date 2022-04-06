f7abe83f5ebf7b39c1f5e55b1566545ac7dba750
fotistsiou <fotis.tsioumas@outlook.com>
2022-04-06 22:25:52 +0300

    antikimenostrefis_programmatismos

--- 2nd_semester/antikimenostrefis_programmatismos/mathima1/unipi/OOP/mathima1/access_modifiers/Main.java
+++ 2nd_semester/antikimenostrefis_programmatismos/mathima1/unipi/OOP/mathima1/access_modifiers/Main.java
@@ -2,7 +2,7 @@ package unipi.OOP.mathima1.access_modifiers;
 
 import unipi.OOP.mathima1.Student; // Import class from other package
 
-public class Main {
+public class Main { // I can create a "Main" class in the different packages, but not in the same package
     public static void main(String[] args) {
         Student student2 = new Student(); // Exists access to "Student" class because it has public access modifier
         // student2.AM = "MPPL56789"; // Does not exists access to "AM" attribute because it has default access modifier
